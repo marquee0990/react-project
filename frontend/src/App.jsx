7f152d2447c883165456eb747473e0b17cf6ca5d
marquee0990 <marcogcastro09@gmail.com>
2026-07-30 09:20:13 -0300

    Estilizando o site

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
-import './App.css'
+import './css/App.css'
 import Favorites from "./pages/Favorites"
 import Home from "./pages/Home"
+import NavBar from "./components/NavBar"
 import {Routes, Route} from "react-router-dom"
 
 // function App() {
@@ -25,13 +26,16 @@ function App() {
 
 
   return (
-  <main className="main-content">
-    <Routes>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/favorites' element={<Favorites/>}></Route>
+  <div>
+    <NavBar/>
+      <main className="main-content">
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/favorites' element={<Favorites/>}></Route>
 
-    </Routes>
-  </main>
+      </Routes>
+    </main>
+  </div>
   )
 }
 
